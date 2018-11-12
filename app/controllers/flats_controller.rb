b4c238ebb220c3a9244b2c0aa88616f5e65f0b8f
Maria <maria.altyeva@gmail.com>
2018-11-12 17:29:47 +0800

    refactored controller code and added image width

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,17 +1,24 @@
 class FlatsController < ApplicationController
   require "open-uri"
 
+  before_action :set_flats, only: [:index, :show]
+
   def index
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
+
   end
 
   def show
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
+
 
     @flat_id = params["id"].to_i
 
     @flat = @flats.find {|flat| @flat_id == flat["id"]}
+  end
+
+  private
+
+  def set_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(open(url).read)
   end
 end
